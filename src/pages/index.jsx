6251645464fb56c90198c0431f9d0ba2b9c945a1
Santiago San Román Játiva <santi72alc@gmail.com>
2021-10-29 23:54:48 +0200

    first commit added src directory

---
+++ src/pages/index.jsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <>
+    <Head>
+      <title>Home page</title>
+    </Head>
+      <h1>Home page new</h1>
+    </>
+  )
+}
